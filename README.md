d1d3ef82c78de9b0aeee412b9fea8fef9c122562
Gyuchan Jeon <gyuchan@lezhin.com>
2018-07-30 19:16:47 +0900

    setup project

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # realm
+이 프로젝트는 realm을 셋팅하고 테스트하기위한 repository입니다.
+
+## setup
+- cocoapods
+- realm
---
+++ realm/realm/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  realm
+//
+//  Created by Gyuchan Jeon on 2018. 7. 30..
+//  Copyright © 2018년 Gyuchan Jeon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
